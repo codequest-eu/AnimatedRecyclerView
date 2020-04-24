37b5bbba2981f5c7041696643f62f3223710fd48
Jakub Komorowski <jakub.komorowski@codequest.com>
2020-04-24 12:28:18 +0200

    Added flag for indicating whether view associated with PostViewHolder was already shown.

--- app/src/main/java/com/codequest/animatedrecyclerview/PostViewHolder.kt
+++ app/src/main/java/com/codequest/animatedrecyclerview/PostViewHolder.kt
@@ -1,29 +1,46 @@
 package com.codequest.animatedrecyclerview
 
+import android.animation.ValueAnimator
 import android.view.View
-import android.view.animation.AlphaAnimation
 import kotlinx.android.synthetic.main.view_post.view.*
 
 class PostViewHolder(view: View) : AnimatedItemHolder(view) {
+    private var animator: ValueAnimator? = null
+    private var isShown = false
 
     fun bind(post: Post) {
         itemView.apply {
             postTitle.text = post.title
             postBody.text = post.body
+
+            alpha = 0f
+            this@PostViewHolder.isShown = false
         }
     }
 
     override fun startShowAnimation() {
-        itemView.startAnimation(AlphaAnimation(itemView.alpha, 1f).apply {
-            duration = 100L
-            fillAfter = true
-        })
+        if (!isShown) {
+            isShown = true
+            animator?.cancel()
+            animator = ValueAnimator.ofFloat(itemView.alpha, 1f).apply {
+                addUpdateListener {
+                    itemView.alpha = it.animatedValue as Float
+                }
+                start()
+            }
+        }
     }
 
     override fun startHideAnimation() {
-        itemView.startAnimation(AlphaAnimation(itemView.alpha, 0f).apply {
-            duration = 100L
-            fillAfter = true
-        })
+        if (isShown) {
+            isShown = false
+            animator?.cancel()
+            animator = ValueAnimator.ofFloat(itemView.alpha, 0f).apply {
+                addUpdateListener {
+                    itemView.alpha = it.animatedValue as Float
+                }
+                start()
+            }
+        }
     }
 }
